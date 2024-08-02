a801521a5d5c6d6de74b34355445af18f4c068ec
killown <systemofdown@gmai.com>
2024-08-02 11:09:53 -0300

    adjusted to work with -h

--- wfctl/main.py
+++ wfctl/main.py
@@ -1,12 +1,13 @@
 import sys
 import json
 from wfctl.ipc import wayfire_commands
+from wfctl.help import usage
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: wfctl <command>")
+    if len(sys.argv) < 2 or "-h" in sys.argv:
+        usage()
         sys.exit(1)
-    
+
     command = ' '.join(sys.argv[1:])
     wayfire_commands(command)
 
